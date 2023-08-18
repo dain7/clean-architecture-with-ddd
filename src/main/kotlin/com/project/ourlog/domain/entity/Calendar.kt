30a039536e1f93f410b79e197cf872697e8cf307
dain7 <dainnnnnn96@gmail.com>
2023-08-18 18:42:01 +0900

    chore: Delete calendar data

--- src/main/kotlin/com/project/ourlog/domain/entity/Calendar.kt
+++
@@ -1,8 +0,0 @@
-package com.project.ourlog.domain.entity
-
-class Calendar (
-        val year : Number,
-        val month : Number,
-        val day : Number,
-) {
-}
--- src/main/kotlin/com/project/ourlog/domain/repository/CalendarRepository.kt
+++
@@ -1,4 +0,0 @@
-package com.project.ourlog.domain.repository
-
-interface CalendarRepository {
-}
