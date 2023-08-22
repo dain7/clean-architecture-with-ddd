d61fd6233c2ef2715cd85a94fa75281a10996639
dain7 <dainnnnnn96@gmail.com>
2023-08-22 15:42:50 +0900

    feat: Add @Transactional

--- src/main/kotlin/com/project/ourlog/domain/service/MemberService.kt
+++ src/main/kotlin/com/project/ourlog/domain/service/MemberService.kt
@@ -4,11 +4,14 @@ import com.project.ourlog.domain.entity.Member
 import com.project.ourlog.domain.repository.MemberDomainRepository
 import com.project.ourlog.domain.usecase.MemberUseCase
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class MemberService (
         private val memberDomainRepository : MemberDomainRepository
 ) : MemberUseCase {
+
+    @Transactional
     override fun signUp(name: String): Member {
         val newMember = Member(name)
         return memberDomainRepository.save(newMember)
